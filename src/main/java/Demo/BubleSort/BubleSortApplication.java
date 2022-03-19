40f7d41b57e328d72a1e12773ad2a0adac24a674
Ilya <to4nonefake@gmail.com>
2022-03-19 21:18:13 +0800

    init

---
+++ src/main/java/Demo/BubleSort/BubleSortApplication.java
@@ -0,0 +1,13 @@
+package Demo.BubleSort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubleSortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubleSortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Demo/BubleSort/BubleSortApplicationTests.java
@@ -0,0 +1,13 @@
+package Demo.BubleSort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BubleSortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
